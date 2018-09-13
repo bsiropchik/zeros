0779e1a9ecda63931f11540eb19b9e7567c1bfed
mookar <mookar@Denics-MacBook-Air.local>
2018-09-13 22:05:33 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let count = 0; 
+   
+    for (let i = 5; number / i >= 1; i *= 5) 
+      count += Math.trunc(number / i); 
+  
+    return count; 
 }
